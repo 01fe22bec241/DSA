ff68698db5fa1b58fc44b61496d4c9a2b9e40a16
Bhagyashri Badakali <130529304+01fe22bec241@users.noreply.github.com>
2024-03-11 21:58:47 +0530

    Add files via upload

---
+++ Conversion of miles to kilometers.c
@@ -0,0 +1,12 @@
+/* Conversion of miles to kilometers */
+#include<stdio.h>
+int main()
+{
+float miles,kilometers;
+printf("Enter distance in miles \n");
+scanf("%f",&miles);
+kilometers=1.6*miles;
+printf("miles in kilometers=%f",kilometers);
+return 0;
+}
+
