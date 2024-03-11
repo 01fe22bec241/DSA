6fccf11f97a7b1a97e63268c61d7619091bc712f
Bhagyashri Badakali <130529304+01fe22bec241@users.noreply.github.com>
2024-03-11 21:34:34 +0530

    Add files via upload

---
+++ maximum of number.c
@@ -0,0 +1,17 @@
+/*Maximum */
+#include<stdio.h>
+int main()
+{
+ int first_num,sec_num,maximum;
+ printf("Enter the 2 numbers\n");
+ scanf("%d%d",&first_num,&sec_num);
+ if(first_num > sec_num)
+ {
+ printf("maximum = first_num");
+ }
+ else
+  {
+  printf("maximum = sec_num");
+  }
+  return 0;
+}
