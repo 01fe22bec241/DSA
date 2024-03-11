1a278242bef2090f963f256fd3fc2dbd3142de47
Bhagyashri Badakali <130529304+01fe22bec241@users.noreply.github.com>
2024-03-11 21:31:09 +0530

    Add files via upload

---
+++ gcd and lcm.c
@@ -0,0 +1,24 @@
+ /* modular program for GCD and LCM */
+ #include<stdio.h>
+ void GCD_LCM(int a,int b)
+ {int rem;
+  int n1 = a;
+  int n2=b;
+  int gcd,lcm;
+   do {
+   rem = a%b;
+   a=b;
+   b=rem;
+  } while(rem!=0);
+  gcd = a;
+  lcm = (n1*n2)/gcd;
+  printf("GCD = %d\n",gcd);
+  printf("lcm = %d\n",lcm);
+  }
+  int main()
+  { int a,b;
+    printf("Enter the two numbers \n");
+    scanf("%d%d",&a,&b);
+    GCD_LCM(a,b);
+    return 0;
+}
