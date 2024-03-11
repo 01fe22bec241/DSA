e2c6e14628b87cfa814bee53603772ece9d7addd
Bhagyashri Badakali <130529304+01fe22bec241@users.noreply.github.com>
2024-03-11 21:28:46 +0530

    Add files via upload

---
+++ Odd or even.c
@@ -0,0 +1,17 @@
+/*To check number ic odd or even */
+#include<stdio.h>
+int main()
+{
+ int N;
+ printf("Enter the number N \n");
+ scanf("%d",&N);
+ if (N%2==0)
+  {
+   printf("N is even number");
+  }
+  else
+   {
+   printf("N is odd number");
+   }
+   return 0;
+}
