945ac2f56a0ed8309dabe323e1e6f2694dd2369a
Bhagyashri Badakali <130529304+01fe22bec241@users.noreply.github.com>
2024-03-15 15:46:58 +0530

    Update add_matrix.c

--- add_matrix.c
+++ add_matrix.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 int main()
 {
-    int a[5]={1,2,3,4,5};
-    int b[5]={1,2,3,4,5};
-    int c[5],i;
+    int a[6]={1,2,3,4,5,6};
+    int b[6]={1,2,3,4,5,6};
+    int c[6],i;
     printf("Total matrix sum is\n");
-    for(i=0;i<5;i++)
+    for(i=0;i<6;i++)
     {
         c[i]=a[i]+b[i];
         printf("%d  ",c[i]);
